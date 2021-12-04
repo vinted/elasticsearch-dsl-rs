ad88adb3d6e7c9e7942bc394d604ed8e0882cd6c
Evaldas Buinauskas <7301441+buinauskas@users.noreply.github.com>
2021-12-04 19:25:56 +0200

    Implement raw JSON query (#57)
    * Implement raw JSON query
    
    * Bump project version

---
+++ src/search/queries/custom/json_query.rs
@@ -0,0 +1,55 @@
+use crate::search::*;
+use crate::util::*;
+
+/// Raw JSON query for something not yet supported.
+///
+/// To create JSON query:
+/// ```
+/// # use elasticsearch_dsl::queries::*;
+/// # use elasticsearch_dsl::queries::params::*;
+/// # let query =
+/// Query::json(serde_json::json!({ "term": { "user": "username" } }));
+/// ```
+/// **NOTE**: This is fallible and can lead to incorrect queries and
+/// rejected search requests, use ar your own risk.
+#[derive(Debug, Clone, PartialEq, Serialize)]
+pub struct JsonQuery(serde_json::Value);
+
+impl Query {
+    /// Creates an instance of [`JsonQuery`]
+    ///
+    /// - `query` - raw JSON query
+    pub fn json(query: serde_json::Value) -> JsonQuery {
+        JsonQuery(query)
+    }
+}
+
+impl From<serde_json::Value> for Query {
+    fn from(value: serde_json::Value) -> Self {
+        Self::Json(JsonQuery(value))
+    }
+}
+
+impl From<serde_json::Value> for JsonQuery {
+    fn from(value: serde_json::Value) -> Self {
+        Self(value)
+    }
+}
+
+impl ShouldSkip for JsonQuery {
+    fn should_skip(&self) -> bool {
+        !self.0.is_object()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    test_serialization! {
+        with_required_fields(
+            Query::json(json!({ "term": { "user": "username" } })),
+            json!({ "term": { "user": "username" } })
+        );
+    }
+}
---
+++ src/search/queries/custom/mod.rs
@@ -0,0 +1,5 @@
+//! Non official queries, such as plugins or raw JSON queries
+
+mod json_query;
+
+pub use self::json_query::*;
