3ed93d38ebe5add4cc167d92c8d66ade465e47fd
Evaldas Buinauskas <evaldas.buinauskas@vinted.com>
2021-12-20 11:34:52 +0200

    Implement Display for Number

--- src/search/params/number.rs
+++ src/search/params/number.rs
@@ -12,6 +12,17 @@ pub enum Number {
     F64(f64),
 }
 
+impl std::fmt::Display for Number {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Number::U64(value) => value.fmt(f),
+            Number::I64(value) => value.fmt(f),
+            Number::F32(value) => value.fmt(f),
+            Number::F64(value) => value.fmt(f),
+        }
+    }
+}
+
 impl From<u8> for Number {
     fn from(value: u8) -> Self {
         Self::U64(value as u64)
