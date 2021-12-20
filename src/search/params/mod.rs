34e0bddfc724f4540d3c8514a5f5366b8083475e
Evaldas Buinauskas <7301441+buinauskas@users.noreply.github.com>
2021-12-20 11:26:53 +0200

    Introduce text type (#62)

--- src/search/params/mod.rs
+++ src/search/params/mod.rs
@@ -4,10 +4,12 @@ mod geo_point;
 mod number;
 mod search;
 mod term;
+mod text;
 mod units;
 
 pub use self::geo_point::*;
 pub use self::number::*;
 pub use self::search::*;
 pub use self::term::*;
+pub use self::text::*;
 pub use self::units::*;
