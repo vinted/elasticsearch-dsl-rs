96b93e068e0693e21bb43dfbc6c2347cdbf5a114
Evaldas Buinauskas <7301441+buinauskas@users.noreply.github.com>
2021-12-22 07:49:19 +0200

    Separate terms from terms lookup query (#73)

--- src/search/queries/term_level/terms_query.rs
+++ src/search/queries/term_level/terms_query.rs
@@ -21,27 +21,17 @@ use std::collections::BTreeSet;
 ///     .boost(2)
 ///     .name("test");
 /// ```
-/// To create a terms lookup query:
-/// ```
-/// # use elasticsearch_dsl::queries::*;
-/// # use elasticsearch_dsl::queries::params::*;
-/// # let query =
-/// Query::terms_lookup("test", "index", "id", "path")
-///     .routing("routing")
-///     .boost(1.3)
-///     .name("lookup");
-/// ```
 /// <https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-terms-query.html>
 #[derive(Debug, Clone, PartialEq, Serialize)]
-pub struct TermsQuery<T: Terms> {
+pub struct TermsQuery {
     #[serde(rename = "terms")]
-    inner: Inner<T>,
+    inner: Inner,
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize)]
-struct Inner<T: Terms> {
+struct Inner {
     #[serde(flatten)]
-    pair: KeyValuePair<String, T>,
+    pair: KeyValuePair<String, BTreeSet<Term>>,
 
     #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     boost: Option<Boost>,
@@ -62,7 +52,7 @@ impl Query {
     /// [`index.max_terms_count setting`](https://www.elastic.co/guide/en/elasticsearch/reference/current/index-modules.html#index-max-terms-count).<br>
     /// > To use the field values of an existing document as search terms,
     /// use the [terms lookup](crate::TermsLookup) parameters.
-    pub fn terms<S, I>(field: S, values: I) -> TermsQuery<BTreeSet<Term>>
+    pub fn terms<S, I>(field: S, values: I) -> TermsQuery
     where
         S: Into<String>,
         I: IntoIterator,
@@ -76,140 +66,52 @@ impl Query {
             },
         }
     }
-
-    /// Creates an instance of [`TermsQuery`]
-    ///
-    /// - `field` - Field you wish to search.
-    /// - `index` - Name of the index from which to fetch field values.
-    /// - `id` - [ID](https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping-id-field.html)
-    /// of the document from which to fetch field values.
-    /// - `path` - Name of the field from which to fetch field values. Elasticsearch uses
-    /// these values as search terms for the query. If the field values
-    /// include an array of nested inner objects, you can access those objects
-    /// using dot notation syntax.
-    pub fn terms_lookup<S: Into<String>>(
-        field: S,
-        index: S,
-        id: S,
-        path: S,
-    ) -> TermsQuery<TermsLookup> {
-        TermsQuery {
-            inner: Inner {
-                pair: KeyValuePair::new(
-                    field.into(),
-                    TermsLookup {
-                        index: index.into(),
-                        id: id.into(),
-                        path: path.into(),
-                        routing: None,
-                    },
-                ),
-                boost: None,
-                _name: None,
-            },
-        }
-    }
 }
 
-impl<T: Terms> TermsQuery<T> {
+impl TermsQuery {
     add_boost_and_name!();
 }
 
-impl TermsQuery<TermsLookup> {
-    /// Custom [routing value](https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping-routing-field.html)
-    /// of the document from which to fetch term values. If a custom routing
-    /// value was provided when the document was indexed, this parameter is
-    /// required.
-    pub fn routing(mut self, routing: impl Into<String>) -> Self {
-        self.inner.pair.value.routing = Some(routing.into());
-        self
-    }
-}
-
-impl ShouldSkip for TermsQuery<BTreeSet<Term>> {
+impl ShouldSkip for TermsQuery {
     fn should_skip(&self) -> bool {
         self.inner.pair.value.should_skip()
     }
 }
 
-impl ShouldSkip for TermsQuery<TermsLookup> {}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    mod term {
-        use super::*;
+    test_serialization! {
+        with_required_fields(
+            Query::terms("test", vec![123, 12, 13]),
+            json!({"terms": { "test": [12, 13, 123] } })
+        );
 
-        test_serialization! {
-            with_required_fields(
-                Query::terms("test", vec![123, 12, 13]),
-                json!({"terms": { "test": [12, 13, 123] } })
-            );
-
-            with_all_fields(
-                Query::terms("test", vec![123]).boost(2).name("test"),
-                json!({
-                    "terms": {
-                        "test": [123],
-                        "boost": 2,
-                        "_name": "test",
-                    }
-                })
-            );
-        }
-
-        #[test]
-        fn should_skip_when_there_are_no_values() {
-            let values: Vec<Term> = Vec::new();
-            let query = Query::terms("test", values);
-
-            assert!(query.should_skip())
-        }
-
-        #[test]
-        fn should_not_skip_when_there_are_no_values() {
-            let query = Query::terms("test", vec![123]);
-
-            assert!(!query.should_skip())
-        }
+        with_all_fields(
+            Query::terms("test", vec![123]).boost(2).name("test"),
+            json!({
+                "terms": {
+                    "test": [123],
+                    "boost": 2,
+                    "_name": "test",
+                }
+            })
+        );
     }
 
-    mod lookup {
-        use super::*;
+    #[test]
+    fn should_skip_when_there_are_no_values() {
+        let values: Vec<Term> = Vec::new();
+        let query = Query::terms("test", values);
 
-        test_serialization! {
-            with_required_fields(
-                Query::terms_lookup("test", "index_value", "id_value", "path_value"),
-                json!({
-                    "terms": {
-                        "test": {
-                            "index": "index_value",
-                            "id": "id_value",
-                            "path": "path_value",
-                        }
-                    }
-                })
-            );
+        assert!(query.should_skip())
+    }
 
-            with_all_fields(
-                Query::terms_lookup("test", "index_value", "id_value", "path_value")
-                    .routing("routing_value")
-                    .boost(2)
-                    .name("test"),
-                json!({
-                    "terms": {
-                        "test": {
-                            "index": "index_value",
-                            "id": "id_value",
-                            "path": "path_value",
-                            "routing": "routing_value"
-                        },
-                        "boost": 2,
-                        "_name": "test",
-                    }
-                })
-            );
-        }
+    #[test]
+    fn should_not_skip_when_there_are_no_values() {
+        let query = Query::terms("test", [123]);
+
+        assert!(!query.should_skip())
     }
 }
