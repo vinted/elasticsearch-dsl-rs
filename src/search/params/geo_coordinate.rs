31ce0d59f3544f50e328a3728964328b71d0fa82
Evaldas Buinauskas <7301441+buinauskas@users.noreply.github.com>
2022-01-01 19:10:36 +0200

    Implement FromStr for coordinate types (#96)

--- src/search/params/geo_coordinate.rs
+++ src/search/params/geo_coordinate.rs
@@ -1,5 +1,5 @@
 use serde::Serialize;
-use std::fmt::Display;
+use std::{fmt::Display, str::FromStr};
 
 /// Represents a point in two dimensional space
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -51,6 +51,25 @@ impl From<(f32, f32)> for GeoCoordinate {
     }
 }
 
+impl FromStr for GeoCoordinate {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut values = s.split(',');
+
+        let longitude = values.next().and_then(|x| x.trim().parse().ok());
+        let latitude = values.next().and_then(|x| x.trim().parse().ok());
+
+        match (longitude, latitude, values.next()) {
+            (Some(longitude), Some(latitude), None) => Ok(Self {
+                longitude,
+                latitude,
+            }),
+            _ => Err(format!("Couldn't parse '{}' as geographic coordinate", s)),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -61,5 +80,21 @@ mod tests {
         assert_serialize(GeoCoordinate::new(1.1, 2.2), json!([2.2, 1.1]));
         assert_serialize(GeoCoordinate::from([2.2, 1.1]), json!([2.2, 1.1]));
         assert_serialize(GeoCoordinate::from((2.2, 1.1)), json!([2.2, 1.1]));
+    }
+
+    #[test]
+    fn from_str() {
+        assert_eq!(
+            GeoCoordinate::from_str("1.1, 2.2").unwrap(),
+            GeoCoordinate::new(2.2, 1.1)
+        );
+        assert_eq!(
+            GeoCoordinate::from_str("1,2").unwrap(),
+            GeoCoordinate::new(2., 1.)
+        );
+
+        assert!(GeoCoordinate::from_str("1.1").is_err());
+        assert!(GeoCoordinate::from_str("1.1,2.2,3").is_err());
+        assert!(GeoCoordinate::from_str("abc").is_err());
     }
 }
