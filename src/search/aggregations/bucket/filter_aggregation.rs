a281eba97ee3702208a317eb2995a673b75b664f
Evaldas Buinauskas <evaldas.buinauskas@vinted.com>
2022-03-21 17:24:58 +0200

    Implement filters aggregation

---
+++ src/search/aggregations/bucket/filter_aggregation.rs
@@ -0,0 +1,56 @@
+use crate::search::*;
+use crate::util::*;
+
+#[derive(Debug, Clone, Serialize, PartialEq)]
+/// A single bucket aggregation that narrows the set of documents to those that match a query.
+///
+/// <https://www.elastic.co/guide/en/elasticsearch/reference/current/search-aggregations-bucket-filter-aggregation.html>
+pub struct FilterAggregation {
+    filter: Query,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
+    aggs: Aggregations,
+}
+
+#[derive(Debug, Clone, Serialize, PartialEq)]
+struct FilterAggregationInner {
+    query: Query,
+}
+
+impl Aggregation {
+    /// Creates an instance of [`FilterAggregation`]
+    ///
+    /// - `query` - query to filter by
+    pub fn filter<Q>(query: Q) -> FilterAggregation
+    where
+        Q: Into<Query>,
+    {
+        FilterAggregation {
+            filter: query.into(),
+            aggs: Aggregations::new(),
+        }
+    }
+}
+
+impl FilterAggregation {
+    add_aggregate!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn serialization() {
+        assert_serialize(
+            Aggregation::filter(Query::term("type", "t-shirt"))
+                .aggregate("sizes", Aggregation::terms("size")),
+            json!({
+                "filter": { "term": { "type": { "value": "t-shirt"} } },
+                "aggs": {
+                    "sizes": { "terms": { "field": "size" } }
+                }
+            }),
+        );
+    }
+}
