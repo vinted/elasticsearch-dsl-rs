4ac172bd07fee1575a4cc1f20be3587e27c12585
Evaldas Buinauskas <evaldas.buinauskas@vinted.com>
2022-07-18 10:53:58 +0300

    Fix enums with incorrectly used untagged variant.

--- src/search/highlight/fragmenter.rs
+++ src/search/highlight/fragmenter.rs
@@ -1,6 +1,6 @@
 /// Specifies how text should be broken up in highlight snippets.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Serialize)]
-#[serde(untagged, rename_all = "snake_case")]
+#[serde(rename_all = "snake_case")]
 pub enum Fragmenter {
     /// Breaks up text into same-sized fragments.
     Simple,
