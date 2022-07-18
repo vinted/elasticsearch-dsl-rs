4431cd99294a1da3a8c5efa7ec84365c505442fd
Evaldas Buinauskas <7301441+buinauskas@users.noreply.github.com>
2022-07-18 17:37:15 +0300

    Add gap policy enum (#179)

--- src/search/aggregations/params/mod.rs
+++ src/search/aggregations/params/mod.rs
@@ -1,9 +1,11 @@
 //! Value types accepted by aggregation clauses
 
 mod aggregation_name;
+mod gap_policy;
 mod rate_mode;
 mod terms_order;
 
 pub use self::aggregation_name::*;
+pub use self::gap_policy::*;
 pub use self::rate_mode::*;
 pub use self::terms_order::*;
