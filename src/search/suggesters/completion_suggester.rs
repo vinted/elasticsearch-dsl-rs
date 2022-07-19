40bb40d947be4a7075ea67e942dbcc3fcbd54781
Evaldas Buinauskas <evaldas.buinauskas@vinted.com>
2022-07-19 10:41:17 +0300

    Implement completion suggester

---
+++ src/search/suggesters/completion_suggester.rs
@@ -0,0 +1,165 @@
+use super::{SuggestContextQuery, SuggestFuzziness, Suggester};
+use crate::util::ShouldSkip;
+use std::collections::BTreeMap;
+
+/// The `completion` suggester provides auto-complete/search-as-you-type functionality. This is a
+/// navigational feature to guide users to relevant results as they are typing, improving search
+/// precision. It is not meant for spell correction or did-you-mean functionality like the `term`
+/// or `phrase` suggesters.
+///
+/// Ideally, auto-complete functionality should be as fast as a user types to provide instant
+/// feedback relevant to what a user has already typed in. Hence, `completion` suggester is
+/// optimized for speed. The suggester uses data structures that enable fast lookups, but are
+/// costly to build and are stored in-memory.
+#[derive(Debug, Clone, PartialEq, Serialize)]
+pub struct CompletionSuggester {
+    prefix: String,
+
+    completion: CompletionSuggesterCompletion,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize)]
+struct CompletionSuggesterCompletion {
+    field: String,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
+    analyzer: Option<String>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
+    fuzzy: Option<SuggestFuzziness>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
+    size: Option<u64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
+    skip_duplicates: Option<bool>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
+    contexts: BTreeMap<String, Vec<SuggestContextQuery>>,
+}
+
+impl Suggester {
+    /// Creates an instance of [CompletionSuggester]
+    pub fn completion<T, U>(field: T, prefix: U) -> CompletionSuggester
+    where
+        T: ToString,
+        U: ToString,
+    {
+        CompletionSuggester {
+            prefix: prefix.to_string(),
+            completion: CompletionSuggesterCompletion {
+                field: field.to_string(),
+                analyzer: None,
+                fuzzy: None,
+                size: None,
+                skip_duplicates: None,
+                contexts: Default::default(),
+            },
+        }
+    }
+}
+
+impl CompletionSuggester {
+    /// Overrides search time analyzer
+    pub fn analyzer<T>(mut self, analyzer: T) -> Self
+    where
+        T: ToString,
+    {
+        self.completion.analyzer = Some(analyzer.to_string());
+        self
+    }
+
+    /// Allows searching with typos and still getting results back
+    pub fn fuzzy(mut self, fuzzy: SuggestFuzziness) -> Self {
+        self.completion.fuzzy = Some(fuzzy);
+        self
+    }
+
+    /// The number of suggestions to return (defaults to `5`)
+    pub fn size(mut self, size: u64) -> Self {
+        self.completion.size = Some(size);
+        self
+    }
+
+    /// Whether duplicate suggestions should be filtered out (defaults to false).
+    pub fn skip_duplicates(mut self, skip_duplicates: bool) -> Self {
+        self.completion.skip_duplicates = Some(skip_duplicates);
+        self
+    }
+
+    /// Adds suggest context to filter or boost by an indexed context
+    pub fn context<T, U>(mut self, context: T, suggest_context_queries: U) -> Self
+    where
+        T: ToString,
+        U: IntoIterator<Item = SuggestContextQuery>,
+    {
+        let _ = self.completion.contexts.insert(
+            context.to_string(),
+            suggest_context_queries.into_iter().collect(),
+        );
+        self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{util::assert_serialize, Fuzziness};
+
+    #[test]
+    fn serializes() {
+        assert_serialize(
+            Suggester::completion("autocomplete_field", "search text"),
+            json!({
+                "prefix": "search text",
+                "completion": {
+                    "field": "autocomplete_field"
+                }
+            }),
+        );
+
+        assert_serialize(
+            Suggester::completion("autocomplete_field", "search text")
+                .analyzer("search_analyzer")
+                .size(10)
+                .skip_duplicates(true)
+                .fuzzy(
+                    SuggestFuzziness::new()
+                        .fuzziness(Fuzziness::Auto)
+                        .min_length(2)
+                        .prefix_length(3)
+                        .transpositions(true)
+                        .unicode_aware(false),
+                )
+                .context(
+                    "place_type",
+                    [
+                        SuggestContextQuery::new("cafe"),
+                        SuggestContextQuery::new("restaurants").boost(2),
+                    ],
+                ),
+            json!({
+                "prefix": "search text",
+                "completion": {
+                    "field": "autocomplete_field",
+                    "size": 10,
+                    "skip_duplicates": true,
+                    "analyzer": "search_analyzer",
+                    "fuzzy": {
+                        "fuzziness": "AUTO",
+                        "min_length": 2,
+                        "prefix_length": 3,
+                        "transpositions": true,
+                        "unicode_aware": false
+                    },
+                    "contexts": {
+                        "place_type": [
+                            { "context": "cafe" },
+                            { "context": "restaurants", "boost": 2 }
+                        ]
+                    }
+                }
+            }),
+        );
+    }
+}
---
+++ src/search/suggesters/suggest_fuzziness.rs
@@ -0,0 +1,62 @@
+use crate::util::ShouldSkip;
+use crate::Fuzziness;
+
+/// Suggester fuzziness parameters
+#[derive(Debug, Default, Clone, Copy, PartialEq, Serialize)]
+pub struct SuggestFuzziness {
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
+    fuzziness: Option<Fuzziness>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
+    min_length: Option<u64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
+    prefix_length: Option<u64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
+    transpositions: Option<bool>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
+    unicode_aware: Option<bool>,
+}
+
+impl SuggestFuzziness {
+    /// Creates a new instance of [SuggestFuzziness]
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    /// The fuzziness factor, defaults to [`AUTO`](Fuzziness::Auto). See [Fuzziness] for allowed
+    /// settings.
+    pub fn fuzziness(mut self, fuzziness: Fuzziness) -> Self {
+        self.fuzziness = Some(fuzziness);
+        self
+    }
+
+    /// If set to `true`, transpositions are counted as one change instead of two, defaults to
+    /// `true`
+    pub fn transpositions(mut self, transpositions: bool) -> Self {
+        self.transpositions = Some(transpositions);
+        self
+    }
+
+    /// Minimum length of the input before fuzzy suggestions are returned, defaults `3`
+    pub fn min_length(mut self, min_length: u64) -> Self {
+        self.min_length = Some(min_length);
+        self
+    }
+
+    /// Minimum length of the input, which is not checked for fuzzy alternatives, defaults to `1`
+    pub fn prefix_length(mut self, prefix_length: u64) -> Self {
+        self.prefix_length = Some(prefix_length);
+        self
+    }
+
+    /// If `true`, all measurements (like fuzzy edit distance, transpositions, and lengths) are
+    /// measured in Unicode code points instead of in bytes. This is slightly slower than raw
+    /// bytes, so it is set to `false` by default.
+    pub fn unicode_aware(mut self, unicode_aware: bool) -> Self {
+        self.unicode_aware = Some(unicode_aware);
+        self
+    }
+}
