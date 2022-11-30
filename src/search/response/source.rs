11aae0cf6ec418af7c980ee6db41efcad7d19225
Evaldas Buinauskas <7301441+buinauskas@users.noreply.github.com>
2022-11-30 09:22:13 +0200

    Allow creating source from value, raw value (#205)

--- src/search/response/source.rs
+++ src/search/response/source.rs
@@ -1,6 +1,6 @@
 use crate::util::ShouldSkip;
 use serde::de::DeserializeOwned;
-use serde_json::value::RawValue;
+use serde_json::{value::RawValue, Value};
 
 /// Document source with delayed deserialization
 #[derive(Clone, Default, Serialize, Deserialize)]
@@ -42,5 +42,39 @@ impl Source {
     /// Creates source from a string
     pub fn from_string(value: String) -> Result<Self, serde_json::Error> {
         RawValue::from_string(value).map(Self)
+    }
+}
+
+impl From<Value> for Source {
+    /// Creates source from a [Value]
+    ///
+    /// Calling expect here because [Value] always represents a valid JSON and it
+    /// _should be safe_ to do so.
+    fn from(value: Value) -> Self {
+        Self(RawValue::from_string(format!("{value}")).expect("valid json"))
+    }
+}
+
+impl From<Box<RawValue>> for Source {
+    fn from(value: Box<RawValue>) -> Self {
+        Self(value)
+    }
+}
+
+impl<'a> From<&'a RawValue> for Source {
+    fn from(value: &'a RawValue) -> Self {
+        Self(value.to_owned())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn creates_from_json_value() {
+        let _ = Source::from(json!({"key": "value"}));
+        let _ = Source::from(json!({"key": ["value", 1, 1.2, true, null, {"key2": "value2"}]}));
+        let _ = Source::from(json!(["one", 2, 3.0, false, null, {}]));
     }
 }
